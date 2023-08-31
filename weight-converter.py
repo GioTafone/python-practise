3be9934722ff35bfe6b3c34851e2998337029b08
Giovanni Tafone <giovanni.tafone@gmail.com>
2023-08-31 09:05:51 +0300

    weight conversion

--- weight-converter.py
+++ weight-converter.py
@@ -4,14 +4,11 @@ weight = int(input_weight)
 input_unit = input('(L)bs or (K)g: ')
 uppercase_conversion = input_unit.upper()
 
-lbs_to_kg = weight / 2.2
-
-
 if uppercase_conversion == 'K':
     weight_kg = weight * 2.2
     print(f'You are {weight_kg} pounds')
 elif uppercase_conversion == 'L':
-    weight_lbs = weight / 2.2
+    weight_lbs = weight * 0.45
     print(f'You are {weight_lbs} kilos')
 else:
     print('Invalid input. Use only "K" or "k" for Kilos and "L" or "l" for pounds')
